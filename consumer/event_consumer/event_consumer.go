006910bb555588847425f15b059680216bc439c1
KorWu <vlahgrah@gmail.com>
2025-06-24 12:37:14 +0300

    добавил импорты

--- consumer/event_consumer/event_consumer.go
+++ consumer/event_consumer/event_consumer.go
@@ -4,6 +4,7 @@ import (
 	"PracticeBot/events"
 	"log"
 	"time"
+	"sync"
 )
 
 type Consumer struct {
@@ -44,22 +45,42 @@ func (c Consumer) Start() error {
 }
 
 /*
-TODO:
-1. Потеря событий: ретраи, возвращение в хранилище, фолбэ, подтверждение
-2. обработка всей пачки: останавливаться после первой ошибки, вести счетчик
-3. параллельная обработка (sync.WaitGroup)
+Потеря событий: ретраи, возвращение в хранилище, фолбэ, подтверждение
 */
 
-func (c Consumer) handleEvents(events []events.Event) error {
-	for _, event := range events {
+func (c Consumer) handleEvents(eventsList []events.Event) error {
+
+	wg := sync.WaitGroup{}
+
+	var cntErrors countErrors
+
+	for _, event := range eventsList {
 		log.Printf("new event: %v", event.Text)
 
-		if err := c.processor.Process(event); err != nil {
-			log.Printf("ERR processor: %s", err.Error())
-
-			continue
+		cntErrors.mu.Lock()
+		if cntErrors.cnt >= 5 {
+			log.Printf("%d events were not processed in a row. Programm stopped", cntErrors.cnt)
+			break
 		}
-	}
+		cntErrors.mu.Unlock()
 
+		wg.Add(1)
+		go func(ev events.Event) {
+			defer wg.Done()
+			err := c.processor.Process(ev)
+			if err != nil {
+				log.Printf("ERR processing: %s", err.Error())
+				cntErrors.mu.Lock()
+				cntErrors.cnt++
+				cntErrors.mu.Unlock()
+			}
+		}(event)
+	}
+	wg.Wait()
 	return nil
+}
+
+type countErrors struct {
+	cnt int
+	mu  sync.Mutex
 }
